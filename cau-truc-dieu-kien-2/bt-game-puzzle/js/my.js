b913a8103ef3c2e1ffd513244f1767ad2bd5364e
Your Name <you@example.com>
2021-08-23 17:03:41 +0700

    ds

--- cau-truc-dieu-kien-2/bt-game-puzzle/js/my.js
+++ cau-truc-dieu-kien-2/bt-game-puzzle/js/my.js
@@ -2,13 +2,13 @@ function changeSrcImage(elementId, number) {
     let imageClick = document.getElementById(elementId);
     let altImage = imageClick.getAttribute('alt')
     if (altImage === 'funny') {
-        document.getElementById(elementId).setAttribute('src', 'image/monkey_part' + number + 'x1.jpeg');
+        document.getElementById(elementId).setAttribute('src', 'image/monkey_part' + number + 'x1.jpg');
         document.getElementById(elementId).setAttribute('alt', 'monkey');
     } else if (altImage === 'monkey') {
-        document.getElementById(elementId).setAttribute('src', 'image/panda_swap_part' + number + 'x1.jpeg');
+        document.getElementById(elementId).setAttribute('src', 'image/panda_swap_part' + number + 'x1.jpg');
         document.getElementById(elementId).setAttribute('alt', 'panda');
     } else {
-        document.getElementById(elementId).setAttribute('src', 'image/funny-cat1_part' + number + 'x1.jpeg');
+        document.getElementById(elementId).setAttribute('src', 'image/funny-cat1_part' + number + 'x1.jpg');
         document.getElementById(elementId).setAttribute('alt', 'funny');
     }
 
