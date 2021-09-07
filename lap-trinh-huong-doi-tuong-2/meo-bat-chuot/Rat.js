1cfa82f2975557fbf64a2ae1589a8f61ff18b424
Your Name <you@example.com>
2021-09-07 22:36:10 +0700

    fds

--- lap-trinh-huong-doi-tuong-2/meo-bat-chuot/Rat.js
+++ lap-trinh-huong-doi-tuong-2/meo-bat-chuot/Rat.js
@@ -6,14 +6,50 @@ class Rat {
         this.status = true;
     }
 
-    sound() {
+    sound(text) {
         alert("chit chit");
     }
 }
 
+class Cat {
+    constructor(_name, _weight, _speed) {
+        this.name = _name;
+        this.weight = _weight;
+        this.maxSpeed = _speed;
+    }
 
+    sound(text) {
+        alert("meo meo");
+    }
 
-let chuot = new Rat("Micky", 10, 30);
-let meo = new Cat("tom", 20, 50);
-meo.catch(rat);
-document.getElementById('_name').innerHTML = chuot.name;
+    catch(rat) {
+        if (this.maxSpeed >= rat.speed) {
+            if (rat.status) {
+                alert("meo " + this.name + " da bat dc chuot " + rat.name);
+            } else {
+                alert('chuot da chet')
+            }
+        } else
+            alert('chuot chay nhanh qua')
+    }
+
+    eat(rat) {
+        if (rat.status) {
+            alert("meo " + this.name + " da an chuot " + rat.name);
+        } else alert(rat.name + " da chet");
+        rat.status = false;
+        this.weight = this.weight + rat.weight;
+    }
+}
+
+let meo = new Cat("Tom", 50, 100);
+let rat = new Rat("Micky", 10, 30);
+document.getElementById('_name').innerHTML = meo.name
+
+function tom_catch() {
+    meo.catch(rat);
+}
+
+function tom_eat() {
+    meo.eat(rat);
+}
